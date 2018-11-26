b5a9d3ac7df8cc28aa598b35e43df04c3adbeef0
Daniel Wiser <dan@wiser.ws>
2018-11-26 15:14:37 +0000

    Removed Settings from user dropdown

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,9 +38,6 @@
             <a class="dropdown-item" href="#">
               <i class="dropdown-icon fe fe-user"></i> Profile
             </a>
-            <a class="dropdown-item" href="#">
-              <i class="dropdown-icon fe fe-settings"></i> Settings
-            </a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="dropdown-icon fe fe-log-out"></i> {{ __('Logout') }}
